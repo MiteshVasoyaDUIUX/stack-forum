1d9051ad1248b03f4c442bdaeb728fd211af31db
mitesh <miteshduiux@gmail.com>
2023-02-02 15:42:55 +0530

    Google OAuth Connected...

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var {notFound, errorHandler} = require('./middleware/middleware');
-
+const auth = require('./auth/auth');
+require('dotenv').config();
+const passport = require('./passport/google')
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,6 +19,8 @@ app.get('/', (req, res) =>{
         message : "Response..."
     })
 })
+
+app.use('/auth', auth);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ server/passport/google.js
@@ -0,0 +1,12 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+}, (accessToken, refreshToken, profile, cb) => {
+    console.log(profile);
+    return cb(new Error('Working...'));
+}
+));
