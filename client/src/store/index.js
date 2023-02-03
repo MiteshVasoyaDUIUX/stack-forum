0ae206989bf51cf8f1414565ba6cff36b02f983b
mitesh <miteshduiux@gmail.com>
2023-02-03 18:27:19 +0530

    :joy: From Changing the States Left

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,8 +2,8 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-  },
-  getters: {
+    token : '',
+    user : null
   },
   mutations: {
   },
--- client/src/views/LoggedIn.vue
+++ client/src/views/LoggedIn.vue
@@ -1,5 +1,4 @@
 <template>
   <div class="about">
-    
   </div>
 </template>
